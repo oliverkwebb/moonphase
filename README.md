3abbaa21dbafa0815dbb3b67370214ecbb14f235
Oliver Webb <oliverwebb8192@gmail.com>
2025-05-31 00:09:49 -0500

    zig

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
-# moonphase - A collection of implementations of an algorithm that gets the moon phase
+# moonphase - Werewolf Early Warning System
 
-Impls:
+This repo is a collection of snippets of code that get the phase of the moon, currently, the implementations are:
 
+* Rust
+* JavaScript
 * C/C++
 * Lua
-* JS/TS
-* Rust
+* Zig
+
+These functions take a time as an input (usually in unix epoch seconds or the languages official way of doing time),
+and return the "age" of the moon in radians, such that `(1-cos(x))/2` returns the illuminated fraction of the moons
+surface, this indirection is needed because across a full cycle, the same illuminated percent appears more than once,
+the first and third quarter are a good example.
+
+All these snippets are based off the algorithm in `moontool`, a GUI program made in the 80s
+by John Walker, which took its algorithms from the book *Practical Astronomy With Your Calculator*.
+
+This project contains directories for each language, to run the test suite for any language,
+go inside that languages folder and run `make`.
+
+---
 
 # Rules for submission of [Favorite Language Here]
 
@@ -15,9 +29,3 @@ Your function must be self-contained, such that someone could copy-paste
 it into their code and not get an error. It also must not effect the
 environment outside it if at all possible (no `#define` or mutating globals).
 It must be as "pure" and self-contained as the language will allow.
-
-## The plumbing rule
-
-Your code should take a time (ideally in the programming languages
-time format) and output a value such that `(1-cos(x)/2`
-is the illuminated fraction of the moons surface.
---
+++ zig/src/main.zig
@@ -0,0 +1,52 @@
+// Adapted from "moontool.c" by John Walker: See http://www.fourmilab.ch/moontool/
+pub fn moonphase(unix_date: f64) f64 {
+  const std = @import("std");
+  const math = std.math;
+  const eccent: f64 = 0.016718; // Eccentricity of Earth's orbit
+  const elonge: f64 = 278.833540; // Ecliptic longitude of the Sun at epoch 1980.0
+  const elongp: f64 = 282.596403;// Ecliptic longitude of the Sun at perigee
+  const torad: f64 = math.pi / 180.0;
+
+  const T = struct {
+  	fn fixangle(d: f64) f64 {
+  		return @mod(((@mod(d,360.0)) + 360.0), 360.0);
+  	}
+  };
+
+  const fixangle = T.fixangle;
+
+  // Calculation of the Sun's position
+  const Day = (unix_date / 86400.0 + 2440587.5) - 2444238.5; // Date within epoch
+  const M = torad * fixangle(((360.0 / 365.2422) * Day) + elonge - elongp); // Convert from perigee co-ordinates to epoch 1980.0
+
+  // Solve equation of Kepler
+  var e = M;
+  var delta = e - eccent * math.sin(e) - M;
+  e = e - delta / (1.0 - eccent * math.cos(e));
+  while (@abs(delta) > 1E-6) {
+    delta = e - eccent * math.sin(e) - M;
+    e = e - delta / (1.0 - eccent * math.cos(e));
+  }
+  var Ec = e;
+  Ec = 2.0 * math.atan(math.sqrt((1.0 + eccent) / (1.0 - eccent)) * math.tan(Ec / 2.0)); //  True anomaly
+
+  const Lambdasun = fixangle(((Ec) * (180.0 / math.pi)) + elongp); // Sun's geocentric ecliptic longitude
+  const ml = fixangle(13.1763966 * Day + 64.975464); // Moon's mean lonigitude at the epoch
+  const MM = fixangle(ml - 0.1114041 * Day - 349.383063); // 349:  Mean longitude of the perigee at the epoch, Moon's mean anomaly
+  const Ev = 1.2739 * math.sin(torad * (2.0 * (ml - Lambdasun) - MM)); // Evection
+  const Ae = 0.1858 * math.sin(M); // Annual equation
+  const MmP = torad * (MM + Ev - Ae - (0.37 * math.sin(M))); // Corrected anomaly
+  const lP = ml + Ev + (6.2886 * math.sin(MmP)) - Ae + (0.214 * math.sin(2.0 * MmP)); // Corrected longitude
+  const lPP = lP + (0.6583 * math.sin(torad * (2.0 * (lP - Lambdasun)))); // True longitude
+  const MoonAge = lPP - Lambdasun; // Age of the Moon in degrees
+
+  return MoonAge * torad;
+}
+
+test moonphase {
+    const std = @import("std");
+	try std.testing.expectApproxEqAbs(((1.0-std.math.cos(moonphase(-178070400.0)))/2.0)*100.0, 1.2, 0.1);
+	try std.testing.expectApproxEqAbs(((1.0-std.math.cos(moonphase(361411200.0) ))/2.0)*100.0,93.6, 0.1);
+	try std.testing.expectApproxEqAbs(((1.0-std.math.cos(moonphase(1704931200.0)))/2.0)*100.0, 0.4, 0.1);
+	try std.testing.expectApproxEqAbs(((1.0-std.math.cos(moonphase(2898374400.0)))/2.0)*100.0,44.2, 0.1);
+}
